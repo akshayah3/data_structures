b36d404e5dd5aa1566b88eda98d3c8f45251addc
akshayah3 <akshaynukala95@gmail.com>
2014-04-30 23:45:57 +0530

    insertion sort algorithm

---
+++ insertionsort.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr 30 23:37:30 2014
+
+@author: akshay
+"""
+
+
+
+def insertion_sort(A):
+    for i in range(1,len(A)):
+        key = A[i]
+        j = i - 1
+        while j >= 0 and A[j] > key:
+            A[j + 1] = A[j]
+            j -= 1
+        A[j + 1] = key
+
+
+if __name__ == "__main__":
+    A = [1,5,9,3,4]
+    print "unsorted(A): " + str(A)
+    insertion_sort(A)
+    print "sorted(A): "+ str(A)
+
