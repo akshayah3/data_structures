bc823820c58fd635a62bdae5935d46a6d53c7923
akshayah3 <akshaynukala95@gmail.com>
2014-05-09 19:59:35 +0530

    binary search

---
+++ binarysearch.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+def binary_search(data, target, low, high):
+    ”””
+    Return True if target is found in indicated portion of a Python list.
+    The search only considers the portion from data[low] to data[high] inclusive.
+    
+    ”””
+    if low > high:
+        return False
+    # interval is empty; no match
+
+    else:
+        mid = (low + high) // 2
+        if target == data[mid]:
+            # found a match
+            return True
+        elif target < data[mid]:
+        # recur on the portion left of the middle
+            return binary search(data, target, low, mid − 1)
+        else:
+        # recur on the portion right of the middle
+            return binary search(data, target, mid + 1, high)
